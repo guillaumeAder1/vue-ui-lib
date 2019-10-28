cf44c89cc9aecd745d354a430ebc97c255efc486
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-10-28 19:14:51 +0000

    init

---
+++ src/components/popup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    :style="style" 
+    class="ui-popup">
+    hello
+  </div>
+</template>
+
+<script>
+import t from 'vue-types'
+export default {
+  name: 'popup',
+  props: {
+    data: t.arrayOf(Object),
+    position: t.object.def({
+      width: '25vw',
+      height: '100vh',
+      
+    })
+  },
+  computed: {
+    style () {
+      return { ...this.position }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.ui-popup{
+  display: flex;
+  position: fixed;
+  right: 0;
+  top: 0;
+  background:gray;
+
+}
+</style>>
