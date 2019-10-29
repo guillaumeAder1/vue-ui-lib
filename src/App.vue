5dc1bae6aca288727910407f6a591b7b6bea02bb
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-10-29 21:29:31 +0000

    open popup programatically

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,28 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <popup></popup>
+    <button @click="open">Open popup</button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import Popup from './components/popup'
+import Popup from '@/components/popup'
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    Popup
+  },
+  methods: {
+    open() {
+      var ComponentClass = Vue.extend(Popup)
+      var instance = new ComponentClass({
+          propsData: { type: 'primary' }
+      })
+      instance.$mount() // pass nothing
+      this.$el.appendChild(instance.$el)
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Popup from './components/popup'
 
 Vue.config.productionTip = false
+
+Vue.component('popup', Popup)
 
 new Vue({
   render: h => h(App),
